9cfa3219c5dc552b7ed5c45cb494e57f556f9919
yan febriansyah <44943550+febriansya@users.noreply.github.com>
2022-06-30 08:49:13 +0700

    fundamental android all about activity

---
+++ app/src/main/java/com/example/fundamental1/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.fundamental1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.button_move_to_activity)
+        btnMoveActivity.setOnClickListener(this)
+        val btnMoveAvtivityWithData: Button = findViewById(R.id.button_move_to_activity_with_data)
+        btnMoveAvtivityWithData.setOnClickListener(this)
+        val btnMoveActivityWithDataObject: Button = findViewById(R.id.btn_move_activity_with_object)
+        btnMoveActivityWithDataObject.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.button_move_to_activity -> {
+                val intent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.button_move_to_activity_with_data -> {
+                val intent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                intent.putExtra(MoveWithDataActivity.EXTRA_NAME, "yan febriansyah")
+                intent.putExtra(MoveWithDataActivity.EXTRA_AGE, 12)
+                startActivity(intent)
+            }
+            R.id.btn_move_activity_with_object -> {
+
+                val person = Person(
+                    name = "yan febriansyah",
+                    12,
+                    "kawo",
+                    "yangagah23@gmai.com",
+                    "jln.kawo, batunyala"
+                )
+                val intent = Intent(this@MainActivity, MoveActivity::class.java)
+                intent.putExtra(MoveActivity.EXTRA_PERSON, person)
+                startActivity(intent)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fundamental1/MoveActivity.kt
@@ -0,0 +1,35 @@
+package com.example.fundamental1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MoveActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_AGE = "extra_age"
+        const val EXTRA_NAME = "extra_name"
+        const val EXTRA_PERSON = "extra_person"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+
+        val dataActivity: TextView = findViewById(R.id.move_with_data)
+
+        val movObjectData: TextView = findViewById(R.id.move_object)
+
+        val name = intent.getStringExtra(EXTRA_NAME)
+        val age = intent.getIntExtra(EXTRA_AGE, 0)
+        val objectPerson = intent.getParcelableExtra<Person>(EXTRA_PERSON) as Person
+
+
+        val text = "Name $name, your age : $age "
+        val objectper =
+            "name ${objectPerson.name}, kance age : ${objectPerson.age}, kance address : ${objectPerson.address}, kance email : ${objectPerson.email}"
+        dataActivity.text = text
+        movObjectData.text = objectper
+
+    }
+}
